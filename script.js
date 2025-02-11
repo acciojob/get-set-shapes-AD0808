6883bac242180e95e2fbba3e32e031d0c93ec709
AD0808 <108204118+AD0808@users.noreply.github.com>
2025-02-11 13:06:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-//complete this code
 class Rectangle {
-	constructor(width,height){
-		this._width = width;
-		this._height = height;
-	}
-	get width(){
-		return this._width;
-	}
-	get height(){
-		return this._height;
-	}
-	getArea(){
-		return (this._width*this._height);
-	}
-	
+  constructor(width, height) {
+    if (width <= 0 || height <= 0) {
+      console.log("Width and height must be positive integers.");
+    }
+    this._width = width;
+    this._height = height;
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
 }
 
 class Square extends Rectangle {
-	constructor(side) {
-	    super(side, side);
+  constructor(side) {
+    if (side <= 0) {
+      console.log("Side must be a positive integer.");
+    }
+    super(side, side);
   }
 
   getPerimeter() {
